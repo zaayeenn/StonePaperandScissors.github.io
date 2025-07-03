3206ffe99d9bc9103a16d7d06387dd020821184a
Zayen <athazahrane@gmail.com>
2025-07-03 23:14:24 +0700

    add alert

--- script.js
+++ script.js
@@ -17,28 +17,64 @@ options.forEach((option) => {
             const computerChoices = ["STONE", "PAPER", "SCISSORS"];
             const computerChoice = computerChoices[Math.floor(Math.random() * 3)];
 
-            // Set image sesuai pilihan
             player.src = "./images/" + playerChoice.toLowerCase() + "Player.png";
             computer.src = "./images/" + computerChoice.toLowerCase() + "Computer.png";
 
-            // Hitung skor
             let cPoints = parseInt(computerPoints.innerHTML);
             let pPoints = parseInt(playerPoints.innerHTML);
 
-            if (playerChoice === "STONE") {
-                if (computerChoice === "PAPER") cPoints++;
-                else if (computerChoice === "SCISSORS") pPoints++;
-            } else if (playerChoice === "PAPER") {
-                if (computerChoice === "SCISSORS") cPoints++;
-                else if (computerChoice === "STONE") pPoints++;
-            } else if (playerChoice === "SCISSORS") {
-                if (computerChoice === "STONE") cPoints++;
-                else if (computerChoice === "PAPER") pPoints++;
+            let result = "";
+
+            if (playerChoice === computerChoice) {
+                result = "DRAW";
+            } else if (
+                (playerChoice === "STONE" && computerChoice === "SCISSORS") ||
+                (playerChoice === "PAPER" && computerChoice === "STONE") ||
+                (playerChoice === "SCISSORS" && computerChoice === "PAPER")
+            ) {
+                pPoints++;
+                playerPoints.innerHTML = pPoints;
+                result = "WIN";
+            } else {
+                cPoints++;
+                computerPoints.innerHTML = cPoints;
+                result = "LOSE";
             }
 
-            computerPoints.innerHTML = cPoints;
-            playerPoints.innerHTML = pPoints;
-
+            // Tampilkan alert + animasi berdasarkan hasil
+            if (result === "WIN") {
+                Swal.fire({
+                    title: "🎉 YOU WIN!",
+                    text: "Great move!",
+                    icon: "success",
+                    timer: 1500,
+                    showConfirmButton: false,
+                    didOpen: () => {
+                        confetti({
+                            particleCount: 150,
+                            spread: 70,
+                            origin: { y: 0.6 }
+                        });
+                    }
+                });
+            } else if (result === "LOSE") {
+                Swal.fire({
+                    title: "😢 YOU LOSE!",
+                    text: "Try again!",
+                    icon: "error",
+                    timer: 1500,
+                    showConfirmButton: false,
+                });
+            } else {
+                Swal.fire({
+                    title: "😐 DRAW!",
+                    text: "It's a tie!",
+                    icon: "info",
+                    timer: 1500,
+                    showConfirmButton: false
+                });
+            }
         }, 900);
+
     });
 });
